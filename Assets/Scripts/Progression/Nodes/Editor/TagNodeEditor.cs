892f4021c1f1ebd4c18cd77c43f5268ddbe459b2
Jakub Kozłowicz <jakubkozlowicz10@gmail.com>
2022-01-02 21:26:45 +0100

    Add progression graph and progression nodes

---
+++ Assets/Scripts/Progression/Nodes/Editor/TagNodeEditor.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using XNodeEditor;
+
+[CustomNodeEditor(typeof(TagNode))]
+public class TagNodeEditor : NodeEditor
+{
+    private TagNode _node;
+    private TagNode Node => _node ??= target as TagNode;
+
+    public override void OnBodyGUI()
+    {
+        serializedObject.Update();
+
+        GUILayout.BeginHorizontal();
+        NodeEditorGUILayout.PortField(new GUIContent("In"), target.GetInputPort("input"), GUILayout.MinWidth(0));
+        NodeEditorGUILayout.PortField(new GUIContent("Out"), target.GetOutputPort("output"), GUILayout.MinWidth(0));
+        GUILayout.EndHorizontal();
+        
+
+        Node.Name = GUILayout.TextField(Node.Name);
+
+        var oldState = Node.collected;
+        Node.collected = GUILayout.Toggle(Node.collected, "Collected");
+        
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    public override int GetWidth() => 160;
+
+    public override Color GetTint()
+    {
+        return Node.State switch
+        {
+            ProgressionTag.TagState.Active => new Color(0.27f, 0.39f, 0.28f),
+            ProgressionTag.TagState.Collected => new Color(0.13f, 0.2f, 0.14f),
+            _ => base.GetTint()
+        };
+    }
+}
---
+++ Assets/Scripts/Progression/Nodes/TagNode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using XNode;
+
+[Serializable]
+[CreateNodeMenu("Progression/Tag", 0)]
+public class TagNode : Node, ProgressionTag
+{
+    [Input]
+    public bool input;
+
+    [Output]
+    public bool output;
+
+    public bool collected;
+
+    [SerializeField]
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public ProgressionTag.TagState State => IsCollected() ? ProgressionTag.TagState.Collected : IsActive() ? ProgressionTag.TagState.Active : ProgressionTag.TagState.Inactive;
+
+    public override object GetValue(NodePort port)
+    {
+        if (port.fieldName == "output")
+            return collected;
+
+        return null;
+    }
+
+    public bool IsActive()
+    {
+        var values = GetInputPort("input").GetInputValues<bool>();
+
+        return values.Length == 0 || MathHelper.Or(values);
+    }
+
+    public bool IsCollected()
+    {
+        return collected;
+    }
+}
