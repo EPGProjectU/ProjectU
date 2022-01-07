6e1559b19f80dafe878b9cb2c93ff65f6eeb193e
s16892 <s16892@pjwstk.edu.pl>
2022-01-07 19:20:22 +0100

    reverted camera to follow player

--- Assets/Scripts/LocalAvoidance.cs
+++ Assets/Scripts/LocalAvoidance.cs
@@ -14,7 +14,12 @@ public class LocalAvoidance : MonoBehaviour
 
     void Update()
     {
+        //could be earlyupdate or late to make sure it is called at ther right time
+
         //raycast and if obstacle present modify waypoint(s) in seeker.Path.vectorPath
-        //for now it is just colliders but that may be buggy 
+        //for now it is just collider physics but that may be buggy
+
+        //https://docs.unity3d.com/ScriptReference/Collider.Raycast.html
+        //https://howtorts.github.io/2014/01/14/avoidance-behaviours.html
     }
 }
