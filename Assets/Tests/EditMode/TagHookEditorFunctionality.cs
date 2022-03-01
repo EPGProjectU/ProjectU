bd9e545dff95bf5fa1dba76a3579e2941df682cd
Jakub Kozłowicz <jakubkozlowicz10@gmail.com>
2022-03-01 11:42:54 +0100

    Add test runs for ProgressionManager

---
+++ Assets/Tests/EditMode/TagHookEditorFunctionality.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TagHookEditorFunctionality
+{
+    private ProgressionGraph _originalGraph;
+
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        ProgressionManager.ResetForTest();
+        var data = Resources.Load<ProgressionManagerData>(ProgressionManager.DataPath);
+
+        _originalGraph = data.graph;
+        data.graph = ScriptableObject.CreateInstance<ProgressionGraph>();
+
+        for (var i = 0; i < 10; ++i)
+        {
+            var tagNode = data.graph.AddNode<TagNode>();
+            tagNode.Name = "TestNode" + i;
+        }
+
+        ProgressionManager.HardRefresh();
+
+        yield return null;
+    }
+
+    [UnityTearDown]
+    public IEnumerator TearDown()
+    {
+        var data = Resources.Load<ProgressionManagerData>(ProgressionManager.DataPath);
+
+        data.graph = _originalGraph;
+
+        foreach (var gameObject in GameObject.FindObjectsOfType(typeof(MonoBehaviour)))
+        {
+            Object.DestroyImmediate(gameObject);
+        }
+        
+        ProgressionManager.HardRefresh();
+
+        yield return null;
+    }
+
+    [Test]
+    public void HookRegistration()
+    {
+        var numberOfHooks = ProgressionManager.NumberOfRegisteredHooks();
+
+        var hook = TagHook.Create("NonExisting");
+
+        Assert.AreEqual(numberOfHooks + 1, ProgressionManager.NumberOfRegisteredHooks());
+        Assert.AreEqual(null, hook.Tag);
+    }
+
+    [Test]
+    public void TagRenamingOfRegisteredHook()
+    {
+        var numberOfHooks = ProgressionManager.NumberOfRegisteredHooks();
+
+
+        var hook0 = TagHook.Create("TestNode0");
+        var hookNonExisting = TagHook.Create("NonExisting");
+
+        Assert.AreEqual("TestNode0", hook0.TagName);
+        Assert.AreEqual("NonExisting", hookNonExisting.TagName);
+
+        var nodes = ProgressionManager.Data.graph.nodes.OfType<TagNode>().ToList();
+
+        foreach (var node in nodes)
+        {
+            node.Name += "_rename";
+        }
+
+        Assert.AreEqual("TestNode0_rename", hook0.TagName);
+        Assert.AreEqual("NonExisting", hookNonExisting.TagName);
+    }
+}
---
+++ Assets/Tests/PlayMode/TagHookPlayModeFunctionality.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Object = UnityEngine.Object;
+
+// ReSharper disable InconsistentNaming
+
+public class TagHookPlayModeFunctionality
+{
+    private ProgressionGraph _originalGraph;
+    
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        // Swapping currently used graph for a new one for duration of test
+        var data = Resources.Load<ProgressionManagerData>(ProgressionManager.DataPath);
+        
+        _originalGraph = data.graph;
+        data.graph =  ScriptableObject.CreateInstance<ProgressionGraph>();
+        
+        // Populating the new graph
+        for (var i = 0; i < 10; ++i)
+        {
+            var tagNode = data.graph.AddNode<TagNode>();
+            tagNode.Name = "TestNode" + i;
+        }
+
+        ProgressionManager.InitForTest();
+        
+        yield return null;
+    }
+
+    [UnityTearDown]
+    public IEnumerator TearDown()
+    {
+        var data = Resources.Load<ProgressionManagerData>(ProgressionManager.DataPath);
+        
+        data.graph = _originalGraph;
+        
+        foreach (var gameObject in Object.FindObjectsOfType(typeof(MonoBehaviour)))
+        {
+            Object.Destroy(gameObject);
+        }
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator HookLinking()
+    {
+        var hook = TagHook.Create("TestNode0");
+        var hookNonExisting = TagHook.Create("NonExisting");
+        
+        Assert.AreEqual(10, ProgressionManager.Data.graph.nodes.Count);
+        Assert.AreSame(ProgressionManager.GetTag(hook.TagName), hook.Tag);
+        Assert.NotNull(hook.Tag);
+        Assert.AreSame(ProgressionManager.GetTag(hookNonExisting.TagName), hookNonExisting.Tag);
+        ProgressionManager.Data.graph.AddNode<TagNode>();
+
+        hook.Release();
+        hookNonExisting.Release();
+        yield return null;
+    }
+
+    private class StartUpInitializedHookClass : MonoBehaviour
+    {
+        private readonly TagHook _hookFieldInit = TagHook.Create("TestNode0");
+        private TagHook _hookOnEnable;
+        private TagHook _hookAwake;
+        private TagHook _hookStart;
+
+        private void OnEnable()
+        {
+            //_hookOnEnable.TagName = "TestNode0";
+            _hookOnEnable = TagHook.Create("TestNode0");
+        }
+
+        private void Awake()
+        {
+            _hookAwake = TagHook.Create("TestNode0");
+        }
+
+        private void Start()
+        {
+            _hookStart = TagHook.Create("TestNode0");
+        }
+
+        private void OnDestroy()
+        {
+            _hookFieldInit.Release();
+            _hookOnEnable.Release();
+            _hookAwake.Release();
+            _hookStart.Release();
+        }
+
+        public void CheckHookOnEnable()
+        {
+            Assert.AreEqual(ProgressionManager.GetTag(_hookOnEnable.TagName), _hookOnEnable.Tag);
+        }
+        
+        public void CheckHookFieldInit()
+        {
+            Assert.AreEqual(ProgressionManager.GetTag(_hookFieldInit.TagName), _hookFieldInit.Tag);
+        }
+
+        public void CheckHookAwake()
+        {
+            Assert.AreEqual(ProgressionManager.GetTag(_hookAwake.TagName), _hookAwake.Tag);
+        }
+
+        public void CheckHookStart()
+        {
+            Assert.AreEqual(ProgressionManager.GetTag(_hookStart.TagName), _hookStart.Tag);
+        }
+    }
+    
+    [UnityTest]
+    public IEnumerator StartUpInitializedHooks()
+    {
+        var hookClass = new GameObject().AddComponent<StartUpInitializedHookClass>();
+
+        yield return null;
+
+        hookClass.CheckHookOnEnable();
+        hookClass.CheckHookAwake();
+        hookClass.CheckHookStart();
+
+        yield return null;
+    }
+}
