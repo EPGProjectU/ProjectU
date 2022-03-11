565a9c8f891bf885a7bb7061771625d5eff62888
s16892 <s16892@pjwstk.edu.pl>
2022-03-11 14:59:29 +0100

    enemy controller cleanup

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,23 +6,24 @@ using UnityEngine;
 
 public class EnemyController : ActorController
 {    
-    public Transform currentTarget;  //change to private and calculate based on AI 
+    public Transform currentTarget;  //change to private and calculate based on AI module
     UnityEngine.AI.NavMeshAgent agent;
-
-
 
     void Start()
     {
         base.Setup();
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        SetupAgent();
+        
+    }
+
+    private void SetupAgent() {
         agent.speed = BaseSpeed;
         agent.updateRotation = false; //rotation to face towards target will be handled by animation system
         agent.updateUpAxis = false;
-        agent.destination = currentTarget.position; 
-
     }
 
-    void Update() {
+    private void Update() {
         // Update agent destination if the target moves one unit
         if (Vector3.Distance(agent.destination, currentTarget.position) > 1.0f) {
             agent.destination = currentTarget.position;
