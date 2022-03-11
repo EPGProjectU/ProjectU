375313667d911b65e5f97368770c0fb32d5e8175
s16892 <s16892@pjwstk.edu.pl>
2022-03-11 14:09:48 +0100

    created nav mesh and updated enemy prefab to make use of it

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,27 +1,31 @@
-using Pathfinding;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class EnemyController : ActorController
-{
-    protected Pathfinder pathfinder;
-    
-    //temporary testing behaviour of enemy is chasing target
-    public Transform currentTarget;
+{    
+    public Transform currentTarget;  //change to private and calculate based on AI 
+    UnityEngine.AI.NavMeshAgent agent;
+
+
 
     void Start()
     {
         base.Setup();
-        //pathfinder needs Seeker component to work
-        pathfinder = new Pathfinder(GetComponent<Seeker>());
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.speed = BaseSpeed;
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        agent.destination = currentTarget.position;
+  
     }
 
     void Update() {
-
-        //pathfinder calculates path and then next velocity vector based on the calculated path and progress on it
-        Vector2 newVelocity = pathfinder.moveAlongPath(transform, currentTarget, this.BaseSpeed);
-        UpdateVelocity(newVelocity);
+        // Update agent destination if the target moves one unit
+        if (Vector3.Distance(agent.destination, currentTarget.position) > 1.0f) {
+            agent.destination = currentTarget.position;
+        }
     }
 }
