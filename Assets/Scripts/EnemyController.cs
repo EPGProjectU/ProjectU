eccd6dfa9de93dac8b941f2130e65e777621570d
s16892 <s16892@pjwstk.edu.pl>
2022-03-12 13:58:30 +0100

    Enemy controller refactor, separate UpdateAgent method

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -12,21 +12,25 @@ public class EnemyController : ActorController
     void Start()
     {
         base.Setup();
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         SetupAgent();
-        
+    }
+
+    void Update() {
+        UpdateAgent();
     }
 
     private void SetupAgent() {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         agent.speed = BaseSpeed;
         agent.updateRotation = false; //rotation to face towards target will be handled by animation system
         agent.updateUpAxis = false;
     }
 
-    private void Update() {
+    private void UpdateAgent() {
         // Update agent destination if the target moves one unit
         if (Vector3.Distance(agent.destination, currentTarget.position) > 1.0f) {
             agent.destination = currentTarget.position;
         }
     }
+
 }
