992cd250373050cc445ff709710c5b54d51821fb
Jakub Kozłowicz <jakubkozlowicz10@gmail.com>
2022-04-21 11:04:09 +0200

    Create quest management classes

---
+++ Assets/Scripts/Quest/QuestManager.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class QuestManager
+{
+    public static QuestManagerData Data { get; private set; }
+
+    private static readonly string ResourceDataPath = "Quest/QuestManagerData";
+
+    public static void CreateQuest()
+    {
+        Assert.IsNotNull(Data, "Quest database is not set");
+
+        var quest = ScriptableObject.CreateInstance<QuestGraph>();
+        quest.name = "Quest";
+
+#if UNITY_EDITOR
+        AssetDatabase.AddObjectToAsset(quest, Data.database);
+#endif
+        Data.database.quests.Add(quest);
+    }
+
+    public static void RemoveQuest(QuestGraph quest)
+    {
+#if UNITY_EDITOR
+        AssetDatabase.RemoveObjectFromAsset(quest);
+#endif
+    }
+
+    /// <summary>
+    /// Creates <see cref="QuestManagerData"/> file if it does not exist
+    /// </summary>
+    private static void CreateDataFile()
+    {
+#if UNITY_EDITOR
+        var fullDataPath = $"Assets/Resources/{ResourceDataPath}.asset";
+
+        Data = AssetDatabase.LoadAssetAtPath<QuestManagerData>(fullDataPath);
+
+        if (Data)
+            return;
+
+        Debug.Log("QuestManagerData does not exist. Creating a new instance.");
+        Directory.CreateDirectory(Path.GetDirectoryName(fullDataPath)!);
+        Data = ScriptableObject.CreateInstance<QuestManagerData>();
+        AssetDatabase.CreateAsset(Data, fullDataPath);
+
+#else
+        LoadData()
+#endif
+    }
+
+    /// <summary>
+    /// Loads <see cref="QuestManagerData"/> from <see cref="ResourceDataPath"/>
+    /// </summary>
+    private static void LoadData()
+    {
+        Data = Resources.Load<QuestManagerData>(ResourceDataPath);
+    }
+
+    static QuestManager() => CreateDataFile();
+}
