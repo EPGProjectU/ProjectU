f5ad483ca4fcc208ea710a34cb501a0a55ee8e44
s16892 <s16892@pjwstk.edu.pl>
2022-04-23 10:33:58 +0200

    Added Sequence.cs

---
+++ Assets/Scripts/AI/BehaviourTree/Sequence.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+namespace BehaviourTree {
+    public class Sequence : BehaviourNode {
+        public Sequence() : base() { }
+
+      
+        //public Sequence(List<Node> children) : base(children) { }
+
+        public override NodeState Evaluate(EnemyController controller) {
+            bool anyChildIsRunning = false;
+
+            foreach (NodePort nodePort in Outputs) {
+
+                BehaviourNode node = null;
+
+                if (nodePort.IsConnected) {
+                    node = nodePort.Connection.node as BehaviourNode;
+
+                    switch (node.Evaluate(controller)) {
+                        case NodeState.FAILURE:
+                            state = NodeState.FAILURE;
+                            return state;
+                        case NodeState.SUCCESS:
+                            continue;
+                        case NodeState.RUNNING:
+                            anyChildIsRunning = true;
+                            continue;
+                        default:
+                            state = NodeState.SUCCESS;
+                            return state;
+                    }
+                }
+            }
+
+            state = anyChildIsRunning ? NodeState.RUNNING : NodeState.SUCCESS;
+            return state;
+        }
+
+    }
+}
+
