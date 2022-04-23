186bd13f4c078ecf28e6ac425e9f4389be6291c6
s16892 <s16892@pjwstk.edu.pl>
2022-04-23 10:32:49 +0200

    Added Checkenemyinfovrange.cs

---
+++ Assets/Scripts/AI/BehaviourTree/CheckEnemyinFOVrange.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BehaviourTree;
+
+public class CheckEnemyInFOVRange : BehaviourNode {
+   
+    public CheckEnemyInFOVRange() {}
+
+    public override NodeState Evaluate(EnemyController controller) {
+      
+        if (controller.target == null) {
+            if (detectEnemy(controller)) {
+                state = NodeState.SUCCESS;
+                return state;
+            }
+            else {
+                state = NodeState.FAILURE;
+                return state;
+            }
+        }
+
+        if (Vector2.Distance(controller.target.position, controller.transform.position) > controller.sightRadius)
+            controller.target = null;
+
+        state = NodeState.SUCCESS;
+        return state;
+
+    }
+
+    private bool detectEnemy(EnemyController controller) {
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(controller.transform.position, controller.sightRadius);
+        
+        if (colliders.Length > 0) {
+
+            foreach (Collider2D collider in colliders) {
+
+                if (collider.gameObject.tag.Equals("Player")) {
+
+                    controller.target = collider.gameObject.transform;
+                    state = NodeState.SUCCESS;
+                    return true;
+                }
+            }
+        }
+        else {
+            return false;
+        }
+
+        return false;
+    }
+
+
+}
