c257963f8e096e1cdf878f23d67f4a52ab1edca8
s16892 <s16892@pjwstk.edu.pl>
2022-04-23 10:27:50 +0200

    Added behaviourtree.cs

---
+++ Assets/Scripts/AI/BehaviourTree/BehaviourTree.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+
+namespace BehaviourTree {
+
+    [CreateAssetMenu(menuName = "ProjectU/AI/BehaviourTree", fileName = "BehaviourTree")]
+    public class BehaviourTree : NodeGraph {
+
+        private BehaviourNode _root;
+
+        public void SetupTree() {
+
+            foreach(BehaviourNode node in nodes) {
+                if (node.IsStartingNode())
+                    _root = node;
+            }
+
+            if (_root == null)
+                throw new Exception("Behaviour Tree must contain Start node");
+
+            //tmp 
+            Debug.Log("Behaviour tree nodes count: " + nodes.Count);
+        }
+
+        public void Evaluate(EnemyController controller) {
+            if (_root != null)
+                _root.Evaluate(controller);
+        }
+    }
+}
