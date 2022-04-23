602c3e8a174af2ffd2c3457b324d52005dcb1934
s16892 <s16892@pjwstk.edu.pl>
2022-04-23 10:34:30 +0200

    Added TaskGoToTarget.cs

---
+++ Assets/Scripts/AI/BehaviourTree/TaskGoToTarget.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BehaviourTree;
+
+public class TaskGoToTarget : BehaviourNode {
+
+    public override NodeState Evaluate(EnemyController controller) {
+        Transform target = controller.target;
+
+        
+        if (target != null && Vector3.Distance(controller.transform.position, target.position) > 0.01f) 
+            controller.agent.SetDestination(target.position);
+
+        state = NodeState.RUNNING;
+        return state;
+    }
+}
