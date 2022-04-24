7d10a57da17c00a0eeeb3ea12a051953cbb7b300
Jakub Kozłowicz <jakubkozlowicz10@gmail.com>
2022-04-24 15:26:09 +0200

    Add QuestEntryPointNode and QuestExitPointNode

---
+++ Assets/Scripts/Quest/Nodes/QuestExitPointNode.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using XNode;
+
+public class QuestExitPointNode : Node, NotifyNodeInterface
+{
+    [Input]
+    public NotifyNodeInterface.EmptyPort input;
+    
+    [Output]
+    public NotifyNodeInterface.EmptyPort output;
+
+    public override object GetValue(NodePort port) => null;
+
+    public bool Notify(object payload)
+    {
+        Debug.Log($"Quest {graph.name} completed.");
+        
+        NotifyNodeHelper.SendNotify(GetOutputPort(nameof(output)), true);
+
+        return true;
+    }
+}
