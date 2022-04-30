e2d18fad8ec678a1bdc4ec53aa6506bff3e998a2
s16892 <s16892@pjwstk.edu.pl>
2022-04-30 14:28:15 +0200

    fixed checkfovinrange class name so the scriptable object instance of it doesn't dissappear in editor

--- Assets/Scripts/AI/BehaviourTree/Leaf/Check/CheckEnemyinFOVrange.cs
+++ Assets/Scripts/AI/BehaviourTree/Leaf/Check/CheckEnemyinFOVrange.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateNodeMenu("BehaviourTree/Leaf/CheckEnemyInFovRange")]
-public class CheckEnemyInFOVRange : LeafNode {
+public class CheckEnemyInFOVrange : LeafNode {
 
 
     public float range;
