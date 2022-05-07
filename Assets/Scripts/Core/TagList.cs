14fac6520580913ab673b0366445658f8bf764eb
Jakub Kozłowicz <jakubkozlowicz10@gmail.com>
2022-05-07 15:16:38 +0200

    Add TagList component

---
+++ Assets/Scripts/Core/TagList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ProjectU.Core
+{
+    [DisallowMultipleComponent]
+    public class TagList : MonoBehaviour, ISerializationCallbackReceiver
+    {
+        public HashSet<string> tagSet = new HashSet<string>();
+
+        ////////////////////////// Serialization Bulshit /////////////////////////////////////
+        [SerializeField]
+        private List<string> tagSerializationList;
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize() => tagSerializationList = tagSet.ToList();
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize() => tagSet = new HashSet<string>(tagSerializationList);
+    }
+}
