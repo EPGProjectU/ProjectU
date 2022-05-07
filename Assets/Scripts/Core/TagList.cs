0a7a3f7040bad039938b5dbe44ba8c1c83a57fbe
Jakub Kozłowicz <jakubkozlowicz10@gmail.com>
2022-05-07 15:19:59 +0200

    Add TagList extension methods to GameObject and Component classes

--- Assets/Scripts/Core/TagList.cs
+++ Assets/Scripts/Core/TagList.cs
@@ -17,4 +17,59 @@ namespace ProjectU.Core
 
         void ISerializationCallbackReceiver.OnAfterDeserialize() => tagSet = new HashSet<string>(tagSerializationList);
     }
+
+    public static class GameObjectTagListExtension
+    {
+        public static void _AddTag(this GameObject gameObject, string tag)
+        {
+            var tagList = gameObject.GetComponent<TagList>();
+
+            if (tagList == null)
+                tagList = gameObject.AddComponent<TagList>();
+            
+            tagList.tagSet.Add(tag);
+        }
+
+        public static void _RemoveTag(this GameObject gameObject, string tag)
+        {
+            var tagList = gameObject.GetComponent<TagList>();
+
+            if (tagList == null)
+                return;
+            
+            tagList.tagSet.Remove(tag);
+        }
+
+        public static bool _CompareTag(this GameObject gameObject, string tag)
+        {
+            var tagList = gameObject.GetComponent<TagList>();
+
+            if (tagList == null)
+                return false;
+            
+            return tagList.tagSet.Contains(tag);
+        }
+
+        public static bool _CompareAnyTag(this GameObject gameObject, IEnumerable<string> tags)
+        {
+            return tags.Any(t => _CompareTag(gameObject, t));
+        }
+
+        public static bool _CompareAllTags(this GameObject gameObject, IEnumerable<string> tags)
+        {
+            return tags.All(t => _CompareTag(gameObject, t));
+        }
+
+        
+        public static void _AddTag(this Component component, string tag) => _AddTag(component.gameObject, tag);
+
+        
+        public static void _RemoveTag(this Component component, string tag) => _RemoveTag(component.gameObject, tag);
+
+        public static bool _CompareTag(this Component component, string tag) => _CompareTag(component.gameObject, tag);
+
+        public static bool _CompareAnyTag(this Component component, IEnumerable<string> tags) => _CompareAnyTag(component.gameObject, tags);
+
+        public static bool _CompareAllTags(this Component component, IEnumerable<string> tags) => _CompareAllTags(component.gameObject, tags);
+    }
 }
