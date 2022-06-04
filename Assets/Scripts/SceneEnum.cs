cf8b530e9d65ef44bc4bce6d7e6928c8170d85cf
s21006 <s21006@pjwstk.edu.pl>
2022-06-04 11:32:23 +0200

    Added scenes from old branch, and created Enum for scenes

---
+++ Assets/Scripts/SceneEnum.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SceneEnum 
+{
+    SampleScene,
+    EquipmentScen, 
+    ItemTester,
+    OptionsMenu,
+    PauseMenu,
+    CreditsScene,
+    LoadMenu,
+    MainMenu 
+}
+
