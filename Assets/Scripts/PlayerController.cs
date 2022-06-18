7e5601c70e8d8cf9359405305423d5ed1dea2798
s21006 <s21006@pjwstk.edu.pl>
2022-06-18 18:33:34 +0200

    Repaired picking up items not removing item

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -269,6 +269,5 @@ public class PlayerController : MonoBehaviour
     private void Pickup(GameObject item)
     {
         this.GetComponent<Equipment>().items.Add(item.GetComponent<ItemDisplay>().item);
-        this.GetComponent<Equipment>().ShowEquipment();
     }
 }
