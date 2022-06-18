5ff63a59188004f447e3181831a4c4cd4167f474
s21006 <s21006@pjwstk.edu.pl>
2022-06-18 16:06:30 +0200

    Added OpenGate script, working example with specific setting for ProgressionTree

---
+++ Assets/OpenGate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGate : MonoBehaviour
+{
+    public TagHook hook;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hook.Tag.IsCollected())
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+    public void CollectTag()
+    {
+        hook.Collect();
+    }
+}
