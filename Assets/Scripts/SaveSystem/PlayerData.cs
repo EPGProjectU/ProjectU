aecf57b3cf7d72347fe40fb706bc33a16ff8ad87
s20152 <s20152@pjwstk.edu.pl>
2022-11-07 21:45:31 +0100

    Add CharacterData class

--- Assets/Scripts/SaveSystem/PlayerData.cs
+++ Assets/Scripts/SaveSystem/PlayerData.cs
@@ -4,13 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class PlayerData 
+public class PlayerData : CharacterData
 {
-    public float health;
-    public float maxHealth;
-    public float armorDurability;
-    public float maximumArmorDurability;
-    public DamageInfo weapon;
-    public Vector3 position;
     public List<String> items = new List<String>();
 }
