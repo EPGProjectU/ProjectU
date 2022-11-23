88f3f9ca7cfbae88909ad4c0eaa0f5e24752d717
Jakub Kozłowicz <jakubkozlowicz10@gmail.com>
2022-11-23 16:03:04 +0100

    Modify path patrol editor
    to treat partial path the same way as invalid path

--- Assets/Scripts/AI/SmartObjects/Editor/PathPatrolDefinitionEditor.cs
+++ Assets/Scripts/AI/SmartObjects/Editor/PathPatrolDefinitionEditor.cs
@@ -20,19 +20,36 @@ public class PathPatrolDefinitionEditor : Editor
         var firstPoint = points.GetArrayElementAtIndex(0).vector3Value;
         var lastPoint = firstPoint;
 
+        // Used for checking which points are invalid
+        var validPaths = new bool[points.arraySize];
 
-        for (var i = 0; i < points.arraySize; i++)
+        // Draws paths
+        for (var i = 1; i < points.arraySize; i++)
         {
             var point = points.GetArrayElementAtIndex(i).vector3Value;
-            points.GetArrayElementAtIndex(i).vector3Value = Handles.FreeMoveHandle(point, Quaternion.identity, 0.2f, Vector3.zero, Handles.SphereHandleCap);
 
-            DrawPath(point, lastPoint);
+            validPaths[i] = DrawPath(point, lastPoint);
 
             lastPoint = point;
         }
 
+        // If path is not looped, last non-existent path is treated as valid
+        validPaths[0] = true;
+
+        // Draw last path if path is looped
         if (serializedObject.FindProperty(nameof(PathPatrolDefinition.loopPath)).boolValue)
-            DrawPath(lastPoint, firstPoint);
+            validPaths[0] = DrawPath(lastPoint, firstPoint);
+
+        // Draw points
+        for (var i = 0; i < points.arraySize; i++)
+        {
+            var point = points.GetArrayElementAtIndex(i).vector3Value;
+
+            // If path from or to the point is invalid, the point is also treated as invalid
+            Handles.color = validPaths[i] & validPaths[(i + 1) % points.arraySize] ? Color.white : Color.red;
+
+            points.GetArrayElementAtIndex(i).vector3Value = Handles.FreeMoveHandle(point, Quaternion.identity, 0.2f, Vector3.zero, Handles.SphereHandleCap);
+        }
 
         if (EditorGUI.EndChangeCheck())
             serializedObject.ApplyModifiedProperties();
@@ -40,25 +57,33 @@ public class PathPatrolDefinitionEditor : Editor
         Handles.color = defaultHandleColor;
     }
 
-
     /// <summary>
     /// Draws patrol path on the NavMesh
     /// </summary>
     /// <param name="startPoint"></param>
     /// <param name="endPoint"></param>
-    private void DrawPath(Vector3 startPoint, Vector3 endPoint)
+    /// <returns>If NavMesh path exist</returns>
+    /// <remarks>If path does not exist red dashed line is drawn between the points</remarks>
+    private static bool DrawPath(Vector3 startPoint, Vector3 endPoint)
     {
-        var _navPath = new NavMeshPath();
+        var navPath = new NavMeshPath();
 
-        var pathExist = NavMesh.CalculatePath(startPoint, endPoint, NavMesh.AllAreas, _navPath);
+        var pathComplete = NavMesh.CalculatePath(startPoint, endPoint, NavMesh.AllAreas, navPath);
 
-        Handles.color = pathExist ? Color.white : Color.red;
-
-        for (int j = 0; j < _navPath.corners.Length - 1; j++)
-        {
-            Handles.DrawLine(_navPath.corners[j], _navPath.corners[j + 1], 3.0f);
-        }
+        pathComplete &= navPath.status == NavMeshPathStatus.PathComplete;
+        
+        Handles.color = pathComplete ? Color.white : Color.red;
 
         Handles.DrawDottedLine(startPoint, endPoint, 3.0f);
+
+        if (!pathComplete)
+            return false;
+
+        for (var j = 0; j < navPath.corners.Length - 1; j++)
+        {
+            Handles.DrawLine(navPath.corners[j], navPath.corners[j + 1], 3.0f);
+        }
+
+        return true;
     }
 }
